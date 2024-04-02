cad7bc87bd774e6ddaeb5cb351be282b2697bebe
Dwiky Rizky Ananditya <kyzsuki@gnuweeb.org>
2024-04-02 08:11:25 +0700

    fix(_admin.tsx): access control issue in session handling

--- src/routes/_admin.tsx
+++ src/routes/_admin.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_admin")({
   beforeLoad: async ({ context: { session, isAdmin } }) => {
-    if (session && !isAdmin) {
+    if (!session || !isAdmin) {
       throw redirect({ to: "/" });
     }
   },
