cdc549fc6daa4bb6aef88d02aa7a4afa42b1e75a
Dwiky Rizky Ananditya <kyzsuki@gnuweeb.org>
2024-04-04 16:45:12 +0700

    refactor(dashboard-header.tsx): home button

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -1,21 +1,21 @@
-import { ArrowLeft } from "lucide-react";
 import { Button } from "./ui/button";
 import { useRouter } from "@tanstack/react-router";
+import { BiHome } from "react-icons/bi";
 
 export default function DashboardHeader() {
-  const route = useRouter();
+  const router = useRouter();
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 hidden md:flex">
           <Button
-            onClick={() => route.history.back()}
+            onClick={() => router.navigate({ to: "/" })}
             variant="ghost"
             className="mr-6 flex items-center gap-2"
           >
-            <ArrowLeft size={20} />
-            Back
+            <BiHome size={20} />
+            Home
           </Button>
         </div>
       </div>
