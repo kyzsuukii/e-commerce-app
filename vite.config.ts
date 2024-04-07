ecc02ac494455cf909ae244d80b5d277d0a3d4bd
Dwiky Rizky Ananditya <kyzsuki@gnuweeb.org>
2024-04-07 13:39:27 +0700

    refactor(vite.config.js): remove unused import

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { RollupOptions } from "rollup";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
