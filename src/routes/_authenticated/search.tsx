6ff26c48a13a522463a98277a1c58138e926ab2c
Dwiky Rizky Ananditya <kyzsuki@gnuweeb.org>
2024-04-08 13:47:34 +0700

    refactor(search.tsx): use data from backend

--- src/routes/_authenticated/search.tsx
+++ src/routes/_authenticated/search.tsx
@@ -1,53 +1,36 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import Loading from "../../components/loading";
-import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import ListProducts from "../../components/ListProducts";
 import { useDebounce } from "@uidotdev/usehooks";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import axios from "axios";
+import useSWR from "swr";
+import { config } from "@/lib/config";
 
 export const Route = createFileRoute("/_authenticated/search")({
-  loader: getCategories,
   component: Search,
   pendingComponent: Loading,
 });
 
-async function getCategories() {
-  try {
-    const response = await axios.get(
-      `https://dummyjson.com/products/categories`
-    );
-    return response.data;
-  } catch (error) {
-    throw new Error(`${error}`);
-  }
-}
-
-async function searchProduct(product: string) {
-  try {
-    const response = await axios.get(
-      `https://dummyjson.com/products/search?q=${encodeURIComponent(product)}`
-    );
-    return response.data;
-  } catch (error) {
-    throw new Error(`${error}`);
-  }
+async function searchProduct(url: string, session: string) {
+  const { data } = await axios.get(`${config.SERVER_API_URL}/v1/${url}`, {
+    headers: {
+      Authorization: `Bearer ${session}`,
+    },
+  });
+  return data;
 }
 
 function Search() {
-  const categories = Route.useLoaderData();
-
+  const { session } = Route.useRouteContext();
   const [query, setQuery] = useState<string>("");
   const q = useDebounce(query, 500);
 
-  const { isLoading, data } = useQuery({
-    queryKey: ["search", q],
-    queryFn: () => searchProduct(q),
-    staleTime: 500,
-    enabled: !!q,
-  });
+  const { isLoading, data } = useSWR(
+    [`product/search?q=${q}`, session],
+    ([url, session]) => searchProduct(url, session)
+  );
 
   return (
     <div className="mt-12 container px-4 mx-auto">
@@ -58,32 +41,21 @@ function Search() {
         placeholder="Search Product"
         className="max-w-3xl mx-auto"
       />
-      <div className="mt-8 flex items-center gap-2 flex-wrap">
-        {categories.map((category: any, index: number) => (
-          <Link
-            to="/category/$categoryName"
-            params={{
-              categoryName: category,
-            }}
-            key={index}
-          >
-            <Button variant="outline">{category}</Button>
-          </Link>
-        ))}
-      </div>
-      {q ? (
-        !isLoading && data.total > 0 ? (
-          <ListProducts products={data.products} />
+      <div className="my-12">
+        {q ? (
+          !isLoading && data ? (
+            <ListProducts products={data} />
+          ) : (
+            <div className="mt-12 flex justify-center text-2xl font-bold">
+              Items not found
+            </div>
+          )
         ) : (
           <div className="mt-12 flex justify-center text-2xl font-bold">
-            Items not found
+            No Items
           </div>
-        )
-      ) : (
-        <div className="mt-12 flex justify-center text-2xl font-bold">
-          No Items
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
